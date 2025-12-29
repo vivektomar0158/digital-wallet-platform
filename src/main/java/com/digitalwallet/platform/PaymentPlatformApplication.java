0c4e6d661a42571e13234cdbef6890e029e7ed24
Vivek <vivektomar0158@gmail.com>
2025-12-29 11:35:52 +0530

    Intial project setup: Spring , Docker, postgres, Redis

---
+++ src/main/java/com/digitalwallet/platform/PaymentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.digitalwallet.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentPlatformApplication.class, args);
+	}
+
+}
